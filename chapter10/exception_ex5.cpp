827314268a160ae9a5bf9af9c95d4abb4ae159c6
dumbo <dumbo@hpz620.homewva.net>
2024-09-04 21:27:48 +0200

    Added chapter 10

---
+++ chapter10/exception_ex5.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Object
+{
+    string d_name;
+
+    public:
+        Object(string name)
+        :
+            d_name(name)
+        {
+            cout << "Constructor of " << d_name << "\n";
+        }
+        Object(Object const &other)
+        :
+            d_name(other.d_name + " (copy)")
+        {
+            cout << "Copy constructor for " << d_name << "\n";
+        }
+        ~Object()
+        {
+            cout << "Destructor of " << d_name << "\n";
+        }
+        void fun()
+        {
+            Object toThrow("'local object'");
+            cout << "Calling fun of " << d_name << "\n";
+            throw toThrow;
+        }
+        void hello()
+        {
+            cout << "Hello by " << d_name << "\n";
+        }
+};
+
+int main()
+{
+    Object out{ "'main object'" };
+    try
+    {
+        out.fun();
+    }
+    catch (Object& o)
+    {
+        cout << "Caught exception\n";
+        o.hello();
+    }
+    // before the following output we see that the destructor of the thrown object is called
+    cout << "After catch" << '\n';
+}
