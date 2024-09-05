1cc3a3a189d85f3f9cab4f210741e7473b5b617e
dumbo <dumbo@hpz620.homewva.net>
2024-09-05 21:42:49 +0200

    Added example ch11

---
+++ chapter11/stringconversionerror.cc
@@ -0,0 +1,48 @@
+    #include <iostream>
+    #include <string>
+    using namespace std;
+
+    struct Insertable
+    {
+        operator int() const
+        {
+            cout << "op int()\n";
+            return 0;
+        }
+    };
+    ostream &operator<<(ostream &out, Insertable const &ins)
+    {
+        return out << "insertion operator";
+    }
+    struct Convertor
+    {
+        operator Insertable() const
+        {
+            return Insertable();
+        }
+    };
+    struct Text
+    {
+        operator int() const
+        {
+            return 1;
+        }
+    };
+    struct Error
+    {
+        operator Text() const
+        {
+            return Text{};
+        }
+    };
+
+    int main()
+    {
+        Insertable insertable;
+        cout << insertable << '\n';
+        Convertor convertor;
+        cout << convertor << '\n';
+        Error error;
+        // This does not compile
+        // cout << error << '\n';
+    }
