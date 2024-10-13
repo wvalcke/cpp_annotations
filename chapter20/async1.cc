7e71fc1f4f3048c31bb3ed63081e788bed1dbc3f
dumbo <dumbo@hpz620.homewva.net>
2024-10-13 21:34:28 +0200

    Added further examples for chapter 20

---
+++ chapter20/async1.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <future>
+
+//code
+bool fun()
+{
+    std::cerr << "    hello from fun\n";
+    return true;
+}
+int exceptionalFun()
+{
+    throw std::exception();
+}
+
+int main()
+try
+{
+    auto fut1 = std::async(std::launch::async, fun);
+    auto fut2 = std::async(std::launch::async, exceptionalFun);
+
+    std::cerr << "fun returned " << std::boolalpha << fut1.get() << '\n';
+    std::cerr << "exceptionalFun did not return " << fut2.get() << '\n';
+}
+catch (...)
+{
+    std::cerr << "caught exception thrown by exceptionalFun\n";
+}
+//=
